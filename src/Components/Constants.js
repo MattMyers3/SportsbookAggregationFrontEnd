e03e97874b08982bb1a6bfc7385780b7b2fac182
NSmizzy <nismith@udel.edu>
2020-08-28 17:44:44 -0400

    Adding error handling if one sportsbook fails. Seems like foxbet is failing pretty consistently

--- src/Components/Constants.js
+++ src/Components/Constants.js
@@ -1,1 +1,1 @@
-export const apiUrl = process.env.NODE_ENV === 'development' ? "https://localhost:5001/api" : "https://gamblingaggregator.azurewebsites.net/api"
+export const apiUrl = "https://sportsbettingaggregation.com/api";
